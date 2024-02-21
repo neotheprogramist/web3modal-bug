5f592a5acaf5ec4e4f483070d0ebc760fbe9dc4c
Neo <neotheprogramist@proton.me>
2024-02-21 10:59:06 +0100

    minimal example

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,34 @@
-import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { component$, useVisibleTask$ } from "@builder.io/qwik";
+import { useLocation, type DocumentHead } from "@builder.io/qwik-city";
+import { defaultWagmiConfig } from "@web3modal/wagmi";
+import { mainnet, arbitrum, type Chain } from "viem/chains";
 
 export default component$(() => {
+  const loc = useLocation();
+
+  // eslint-disable-next-line qwik/no-use-visible-task
+  useVisibleTask$(() => {
+    const projectId = import.meta.env.PUBLIC_PROJECT_ID;
+
+    const metadata = {
+      name: "Web3Modal",
+      description: "Web3Modal Example",
+      url: loc.url.origin,
+      icons: ["https://avatars.githubusercontent.com/u/37784886"],
+    };
+
+    const chains: [Chain, ...Chain[]] = [mainnet, arbitrum];
+    const config = defaultWagmiConfig({
+      chains, // required
+      projectId, // required
+      metadata, // required
+      enableWalletConnect: true, // Optional - true by default
+      enableInjected: true, // Optional - true by default
+      enableEIP6963: true, // Optional - true by default
+      enableCoinbase: true, // Optional - true by default
+    });
+  });
+
   return (
     <>
       <h1>Hi 👋</h1>
