74767a6da675f8eba5449829f2f0a7eb8a781eec
macoveiciprian <ciprian.macovei@n-able.com>
2024-03-01 11:56:57 +0200

    update: v4 web3modal

--- src/components/wallet-connect/index.tsx
+++ src/components/wallet-connect/index.tsx
@@ -1,33 +1,26 @@
 import { component$, $, noSerialize, useStore } from '@builder.io/qwik';
-import {
-    EthereumClient,
-    w3mConnectors,
-    w3mProvider,
-} from "@web3modal/ethereum";
-import { configureChains, createConfig } from "@wagmi/core";
-import { arbitrum, mainnet, polygon, sepolia } from "@wagmi/core/chains";
-import { Web3Modal } from "@web3modal/html";
 
-export const chains = [arbitrum, mainnet, polygon, sepolia];
-const { publicClient } = configureChains(chains, [
-    w3mProvider({ projectId: "26aebf5aa6f44cc2a8f74e674e0617b9" }),
-]);
-const wagmiConfig = createConfig({
-    autoConnect: true,
-    connectors: w3mConnectors({
-        projectId: "26aebf5aa6f44cc2a8f74e674e0617b9",
-        chains,
-    }),
-    publicClient,
-});
+import { arbitrum, mainnet, polygon, sepolia } from "@wagmi/core/chains";
+import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi';
+
+const chains = [arbitrum, mainnet, polygon, sepolia];
+const projectId = "26aebf5aa6f44cc2a8f74e674e0617b9";
+
+const metadata = {
+    name: 'Web3Modal',
+    description: 'Web3Modal Example',
+    url: 'https://web3modal.com',
+    icons: ['https://avatars.githubusercontent.com/u/37784886']
+  }
 
 export const returnWeb3ModalAndClient = async () => {
-    const ethereumClient = new EthereumClient(wagmiConfig, chains);
-    const web3modal = new Web3Modal(
-        { projectId: "26aebf5aa6f44cc2a8f74e674e0617b9", themeMode: "dark" },
-        ethereumClient
-    );
-    return { web3modal, ethereumClient };
+    const config = defaultWagmiConfig({ chains, projectId, metadata })
+    const modal = createWeb3Modal({
+        wagmiConfig: config,
+        projectId,
+        enableAnalytics: true // Optional - defaults to your Cloud configuration
+    })
+    return { modal };
 };
 
 
@@ -38,14 +31,13 @@ export const WalletConnect = component$(() => {
     })
 
     const setWeb3Modal = $(async () => {
-        const { web3modal, ethereumClient } = await returnWeb3ModalAndClient();
-        state.web3modal = noSerialize(web3modal);
-        state.ethereumClient = noSerialize(ethereumClient);
+        const { modal } = await returnWeb3ModalAndClient();
+        state.web3modal = noSerialize(modal);
     });
 
     const openWeb3Modal = $(async () => {
         await setWeb3Modal();
-        await state.web3modal.openModal();
+        await state.web3modal.open();
     })
 
     return <>
